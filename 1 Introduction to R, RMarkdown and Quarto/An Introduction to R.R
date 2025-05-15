3e2049f89db9edf9751e0298dc8b023e83331c2f
merterisen <57435054+merterisen@users.noreply.github.com>
2025-05-15 18:23:27 +0200

    intro

---
+++ 1 Introduction to R, RMarkdown and Quarto/An Introduction to R.R
@@ -0,0 +1,8 @@
+#Install and load contributed packages:
+#install.packages("dplyr")                     #just once (unless you update R)
+#update.packages()                           #occasionally, to get the latest package updates
+
+# load the necessary packages
+library(dplyr)
+getwd()
+
